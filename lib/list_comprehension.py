ec024781adf3ca2878f861964944294bedead825
Cheriroh <prudencec2019@gmail.com>
2025-02-25 15:29:02 +0300

    test passed

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
-def return_evens(num_list):
-    pass
+def return_evens(seq):
+    """Returns a list of even numbers from the given sequence."""
+    return [num for num in seq if num % 2 == 0]
 
-def make_exclamation(sentence_list):
-    pass
+
+
+def make_exclamation(sentences):
+    """Returns a list of sentences with an exclamation mark at the end."""
+    return [sentence + "!" for sentence in sentences]
+
